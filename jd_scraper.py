71ab8f2253e3ce3f10a629c0e56f769761785c6d
Dvij Parekh <dvijparekh1995@gmail.com>
2019-04-20 20:09:00 +0530

    get phone number according to html element classes

--- jd_scraper.py
+++ jd_scraper.py
@@ -1,115 +1,153 @@
 from bs4 import BeautifulSoup
-import urllib
+import urllib.request
 import csv
 
 
 def innerHTML(element):
     return element.decode_contents(formatter="html")
 
+
 def get_name(body):
-	return body.find('span', {'class':'jcn'}).a.string
+    return body.find('span', {'class': 'jcn'}).a.string
+
 
 def get_phone_number(body):
-	try:
-		return body.find('p', {'class':'contact-info'}).span.a.string
-	except AttributeError:
-		return ''
+    try:
+        phone = decode_phonenumber(body.find('p', {'class': 'contact-info'}).span.a)
+        return phone
+        #return body.find('p', {'class': 'contact-info'}).span.a.string
+    except AttributeError:
+        return ''
+
+#this function uses class of html element to get phone number
+#if phone number doesnt get scraped please change classes used below according to number
+def decode_phonenumber(phone):
+    try:
+        main_phone = ""
+        for span_tag in phone:
+        	#you can use switch here
+            main_class = span_tag.attrs['class']
+            if 'icon-dc' in main_class:
+                main_phone += "+"
+            if 'icon-fe' in main_class:
+                main_phone += "("
+            if 'icon-hg' in main_class:
+                main_phone += ")"
+            if 'icon-yz' in main_class:
+                main_phone += "1"
+            if 'icon-wx' in main_class:
+                main_phone += "2"
+            if 'icon-vu' in main_class:
+                main_phone += "3"
+            if 'icon-ts' in main_class:
+                main_phone += "4"
+            if 'icon-rq' in main_class:
+                main_phone += "5"
+            if 'icon-po' in main_class:
+                main_phone += "6"
+            if 'icon-nm' in main_class:
+                main_phone += "7"
+            if 'icon-lk' in main_class:
+                main_phone += "8"
+            if 'icon-ji' in main_class:
+                main_phone += "9"
+            if 'icon-acb' in main_class:
+                main_phone += "0"
+        print(main_phone)
+        return main_phone
+    except:
+        return ''
+
 
 def get_rating(body):
-	rating = 0.0
-	text = body.find('span', {'class':'star_m'})
-	if text is not None:
-		for item in text:
-			rating += float(item['class'][0][1:])/10
+    rating = 0.0
+    text = body.find('span', {'class': 'star_m'})
+    if text is not None:
+        for item in text:
+            rating += float(item['class'][0][1:]) / 10
 
-	return rating
+    return rating
+
 
 def get_rating_count(body):
-	text = body.find('span', {'class':'rt_count'}).string
+    text = body.find('span', {'class': 'rt_count'}).string
 
-	# Get only digits
-	rating_count =''.join(i for i in text if i.isdigit())
-	return rating_count
+    # Get only digits
+    rating_count = ''.join(i for i in text if i.isdigit())
+    return rating_count
+
 
 def get_address(body):
-	return body.find('span', {'class':'mrehover'}).text.strip()
+    return body.find('span', {'class': 'mrehover'}).text.strip()
+
 
 def get_location(body):
-	text = body.find('a', {'class':'rsmap'})
-	if text == None:
-		return
-	text_list = text['onclick'].split(",")
-	
-	latitutde = text_list[3].strip().replace("'", "")
-	longitude = text_list[4].strip().replace("'", "")
-	
-	return latitutde + ", " + longitude
+    text = body.find('a', {'class': 'rsmap'})
+    if text == None:
+        return
+    text_list = text['onclick'].split(",")
+
+    latitutde = text_list[3].strip().replace("'", "")
+    longitude = text_list[4].strip().replace("'", "")
+
+    return latitutde + ", " + longitude
+
 
 page_number = 1
 service_count = 1
 
-
 fields = ['Name', 'Phone', 'Rating', 'Rating Count', 'Address', 'Location']
-out_file = open('Readymade-Garment-Retailers_agra.csv','w')
+out_file = open('data.csv', 'w')
 csvwriter = csv.DictWriter(out_file, delimiter=',', fieldnames=fields)
 
 # Write fields first
-#csvwriter.writerow(dict((fn,fn) for fn in fields))
+# csvwriter.writerow(dict((fn,fn) for fn in fields))
 
 while True:
 
-	# Check if reached end of result
-	if page_number > 50:
-		break
+    # Check if reached end of result
+    if page_number > 50:
+        break
 
-	url="https://www.justdial.com/Agra/Readymade-Garment-Retailers/nct-10401947/page-%s" % (page_number)
-	req = urllib.request.Request(url, headers={'User-Agent' : "Magic Browser"}) 
-	page = urllib.request.urlopen( req )
-	# page=urllib2.urlopen(url)
+    url = "https://www.justdial.com/Agra/Readymade-Garment-Retailers/nct-10401947/page-%s" % (page_number)
+    req = urllib.request.Request(url, headers={'User-Agent': "Magic Browser"})
+    page = urllib.request.urlopen(req)
+    # page=urllib2.urlopen(url)
 
-	soup = BeautifulSoup(page.read(), "html.parser")
-	services = soup.find_all('li', {'class': 'cntanr'})
+    soup = BeautifulSoup(page.read(), "html.parser")
+    services = soup.find_all('li', {'class': 'cntanr'})
 
+    # Iterate through the 10 results in the page
+    for service_html in services:
 
-	# Iterate through the 10 results in the page
-	for service_html in services:
+        # Parse HTML to fetch data
+        dict_service = {}
+        name = get_name(service_html)
+        phone = get_phone_number(service_html)
+        rating = get_rating(service_html)
+        count = get_rating_count(service_html)
+        address = get_address(service_html)
+        location = get_location(service_html)
+        if name != None:
+            dict_service['Name'] = name
+        if phone != None:
+            print('getting phone number')
+            dict_service['Phone'] = phone
+        if rating != None:
+            dict_service['Rating'] = rating
+        if count != None:
+            dict_service['Rating Count'] = count
+        if address != None:
+            dict_service['Address'] = address
+        if location != None:
+            dict_service['Address'] = location
 
-		# Parse HTML to fetch data
-		dict_service = {}
-		name = get_name(service_html)
-		phone = get_phone_number(service_html)
-		rating = get_rating(service_html)
-		count = get_rating_count(service_html)
-		address = get_address(service_html)
-		location = get_location(service_html)
-		if name != None:
-			dict_service['Name'] = name
-		if phone != None:
-			print('getting phone number')
-			dict_service['Phone'] = phone
-		if rating != None:
-			dict_service['Rating'] = rating
-		if count != None:
-			dict_service['Rating Count'] = count
-		if address != None:
-			dict_service['Address'] = address
-		if location != None:
-			dict_service['Address'] = location
+        # Write row to CSV
+        csvwriter.writerow(dict_service)
 
-		# Write row to CSV
-		csvwriter.writerow(dict_service)
+        print("#" + str(service_count) + " ", dict_service)
+        service_count += 1
 
-		print("#" + str(service_count) + " " , dict_service)
-		service_count += 1
-
-	page_number += 1
+    page_number += 1
 
 out_file.close()
-
-
-
-
-
-
-
-
